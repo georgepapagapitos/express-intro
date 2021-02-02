4bbec912dbda4e5c5d7b10009706d6f4ac505570
George Papagapitos <george.g.papagapitos@gmail.com>
2021-02-02 16:08:41 -0600

    added start script to package.json

--- server/server.js
+++ server/server.js
@@ -9,18 +9,6 @@ const port = 3000;
 // Share any files inside the 'public' folder
 app.use(express.static('server/public'));
 
-// app.get('/', function (request, response) {
-//   response.send('<h1>Hello, world!</h1>');
-// });
-
-// app.get('/contact', function (req, res) {
-//   res.send('<h1>Contact me at george.g.papagapitos@gmail.com</h1>');
-// });
-
-// app.get('/about', function (req, res) {
-//   res.send('<h2>I am ready to code on occasion!</h2>');
-// });
-
 // Listen for network requests
 app.listen(port, function () {
   // When the server is ready, call this function
