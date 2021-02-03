50df1c7864859e9dffde8d097a3cd6fec0911b51
George Papagapitos <george.g.papagapitos@gmail.com>
2021-02-03 10:51:31 -0600

    added post method

---
+++ server/modules/quotes.js
@@ -0,0 +1,33 @@
+let index = 0;
+const quotesData = [
+  {
+    quote:
+      "I'm not going to school just for the academics - I wanted to share ideas, to be around people who are passionate about learning.",
+    author: 'Emma Watson',
+  },
+  {
+    quote:
+      "Remember there's no such thing as a small act of kindness. Every act creates a ripple with no logical end.",
+    author: 'Scott Adams',
+  },
+  {
+    quote: 'Intelligence plus character-that is the goal of true education.',
+    author: 'Martin Luther King, Jr.',
+  },
+];
+
+function getNextQuote() {
+  const result = quotesData[index];
+  index += 1;
+  index = index % quotesData.length; // Ensures the API always returns a value
+  console.log(result);
+  return result;
+}
+
+function addQuote(quoteData) {
+  quotesData.push(quoteData);
+  console.log('Number of new quotes', quotesData.length);
+  return true;
+}
+
+module.exports = { getNextQuote, addQuote };
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 // console.log('express', express);
+const quotes = require('./modules/quotes');
 
 // Create our app
 const app = express();
@@ -8,6 +9,23 @@ const port = 3000;
 
 // Share any files inside the 'public' folder
 app.use(express.static('server/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/quotes', function (request, response) {
+  // Logic to handle API request
+  console.log('GET request');
+  response.send(quotes.getNextQuote());
+});
+
+app.post('/quotes', function (request, response) {
+  let quote = request.body.quote_to_add;
+  console.log('author', quote.author);
+  console.log('quote', quote.quote);
+
+  quotes.addQuote(quote);
+  response.sendStatus(200);
+});
 
 // Listen for network requests
 app.listen(port, function () {
